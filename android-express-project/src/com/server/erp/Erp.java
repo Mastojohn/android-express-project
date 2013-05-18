2ecff06cb46e529ec8e91fd4659ea4bee63a3c38
ain <ambroise.dhenain@gmail.com>
2013-05-18 13:22:00 +0000

    Création de la fonction (fonctionnelle ^_^) permettant de récupérer le contenu du fichier XML et le retournant sous forme de buffer string, j'arrête là pour cette partie :p J'ai erré.

--- android-express-project/src/com/server/erp/Erp.java
+++ android-express-project/src/com/server/erp/Erp.java
@@ -1,5 +1,7 @@
 package com.server.erp;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -11,12 +13,13 @@ public class Erp {
 	 * Users who are authorized to access to the app.
 	 */
 	private static final String[] USERS_AUTH = new String[] {
-			"livreur@gmail.com:livreur", "ambroise@gmail.com:ambroise",
+			"livreur@gmail.com:livreur", "ambroise@gmail.com:amb",
 			"leo@gmail.com:leo", "julien@gmail.com:julien" };
 
 	/**
 	 * Check if the couple email/password is correct for access to the app.
 	 * [Simulation]
+	 * 
 	 * @param email
 	 * @param password
 	 * @return boolean
@@ -35,16 +38,35 @@ public class Erp {
 	}
 
 	/**
-	 * Get the rounds of the day for the connected user.
-	 * [Simulation]
+	 * Get the rounds of the day for the connected user. [Simulation]
+	 * 
 	 * @param context
 	 * @return InputStream who contains the readable XML file.
 	 */
-	public static InputStream getRoundsByUser(Context context) {
+	public static StringBuffer getRoundsByUser(Context context) {
 		try {
-			return context.getAssets().open("file:///assets/erp.xml");
+			InputStream input = context.getAssets().open("erp.xml");
+
+			StringBuffer content = new StringBuffer();
+			int value;
+
+			while ((value = input.read()) != -1) {
+				// Write into the content the content read.
+				content.append((char) value);
+			}
+
+			return content;
+		} catch (FileNotFoundException e) {
+			Toast.makeText(
+					context,
+					"Impossible de récupérer le fichier de configuration de votre tournée.\n"
+							+ e.getMessage(), Toast.LENGTH_LONG).show();
+			return null;
 		} catch (IOException e) {
-			Toast.makeText(context, "Impossible de récupérer le fichier de configuration de votre tournée.", Toast.LENGTH_LONG).show();
+			Toast.makeText(
+					context,
+					"Impossible d'ouvrir le fichier de configuration de votre tournée.\n"
+							+ e.getMessage(), Toast.LENGTH_LONG).show();
 			return null;
 		}
 	}
