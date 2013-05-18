648c34012ead2b07d987e804f8bac534127fb17d
ambroise.dhenain@gmail.com <ambroise.dhenain@gmail.com@1c1b8a7e-04e1-d7a9-97cf-1652473ac422>
2013-05-18 13:22:00 +0000

    Création de la fonction (fonctionnelle ^_^) permettant de récupérer le contenu du fichier XML et le retournant sous forme de buffer string, j'arrête là pour cette partie :p J'ai erré.

--- trunk/android-express-project/src/com/app/express/activity/NextDelivery.java
+++ trunk/android-express-project/src/com/app/express/activity/NextDelivery.java
@@ -1,11 +1,14 @@
 package com.app.express.activity;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.app.express.R;
 import com.server.erp.Erp;
@@ -18,7 +21,12 @@ public class NextDelivery extends Activity {
 		setContentView(R.layout.activity_next_delivery);
 		
 		// Get the round xml file from the ERP. (Simulation)
-		InputStream rounds = Erp.getRoundsByUser(this);
+		StringBuffer rounds = Erp.getRoundsByUser(this);
+		if(rounds != null){
+			// Rounds are available. Display content.
+			Toast.makeText(this, rounds.toString(), Toast.LENGTH_LONG)
+					.show();
+		}
 	}
 
 	@Override
