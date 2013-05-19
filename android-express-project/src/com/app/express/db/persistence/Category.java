a28e4fddec61bbdc754b14bb9363b2d498c8b51a
ain <ambroise.dhenain@gmail.com>
2013-05-19 02:20:01 +0000

    Nombreux tests, ajout d'une librairie, Roboguice mais problèmes d'utilisation.
    https://github.com/roboguice/roboguice/wiki
    https://github.com/roboguice/roboguice/wiki/InstallationNonMaven
    https://github.com/roboguice/roboguice/issues/29

--- android-express-project/src/com/app/express/db/persistence/Category.java
+++ android-express-project/src/com/app/express/db/persistence/Category.java
@@ -1,24 +1,14 @@
 package com.app.express.db.persistence;
 
-import java.sql.SQLException;
-
-
-import android.content.Context;
-import android.util.Log;
-
 import com.app.express.config.Categories;
-import com.app.express.db.DatabaseHelper;
 import com.app.express.db.dao.DeliveryDao;
-import com.google.inject.Inject;
-import com.google.inject.Provider;
-import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.field.DataType;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.misc.BaseDaoEnabled;
 import com.j256.ormlite.table.DatabaseTable;
 
 @DatabaseTable(tableName = "delivery", daoClass = DeliveryDao.class)
-public class Delivery extends BaseDaoEnabled {
+public class Category extends BaseDaoEnabled {
 	@DatabaseField(generatedId = true, columnName = "delivery_id", dataType = DataType.INTEGER_OBJ)
 	private Integer deliveryId;
 	
@@ -34,40 +24,31 @@ public class Delivery extends BaseDaoEnabled {
 	@DatabaseField(columnName = "type_delivery", dataType = DataType.STRING, canBeNull = false, defaultValue = Categories.Types.TypeDelivery.DELIVERY, index = true)
 	private String typeDelivery;
 
-	public Delivery() {
+	public Category() {
 		// ORMLite needs a no-arg constructor
 	}
 
-	public Delivery(Integer roundId, Integer senderId, Integer priority) {
+	public Category(Integer roundId, Integer senderId, Integer priority) {
 		this.roundId = roundId;
 		this.senderId = senderId;
 		this.priority = priority;
 	}
 	
-	public Delivery(Dao dao, Integer roundId, Integer senderId, Integer priority) {
+	public Category(Integer deliveryId, Integer roundId, Integer senderId, Integer priority) {
+		this.deliveryId = deliveryId;
 		this.roundId = roundId;
 		this.senderId = senderId;
 		this.priority = priority;
-		
-		this.setDao(dao);
 	}
 	
-	public Delivery(Integer roundId, Integer senderId, Integer priority, String typeDelivery) {
-		this.roundId = roundId;
-		this.senderId = senderId;
-		this.priority = priority;
-		this.typeDelivery = typeDelivery;
-	}
-	
-	public Delivery(Dao dao, Integer roundId, Integer senderId, Integer priority, String typeDelivery) {
+	public Category(Integer deliveryId, Integer roundId, Integer senderId, Integer priority, String typeDelivery) {
+		this.deliveryId = deliveryId;
 		this.roundId = roundId;
 		this.senderId = senderId;
 		this.priority = priority;
 		this.typeDelivery = typeDelivery;
-		
-		this.setDao(dao);
 	}
-	
+
 	public Integer getDeliveryId() {
 		return deliveryId;
 	}
---
+++ android-express-project/src/com/app/express/helper/App.java
@@ -0,0 +1,10 @@
+package com.app.express.helper;
+
+import com.app.express.db.DatabaseHelper;
+
+import android.content.Context;
+
+public class App {
+	public static Context context;
+	public static DatabaseHelper dbHelper;
+}
