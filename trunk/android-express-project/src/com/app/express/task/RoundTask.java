15892906d4457f9436bd5869c569604542e30678
ambroise.dhenain@gmail.com <ambroise.dhenain@gmail.com@1c1b8a7e-04e1-d7a9-97cf-1652473ac422>
2013-05-22 22:56:27 +0000

    Géolocalisation fonctionnelle, dessin d'un parcours test fonctionnel, 2 points.

---
+++ trunk/android-express-project/src/com/app/express/task/RoundTask.java
@@ -0,0 +1,216 @@
+package com.app.express.task;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.app.express.helper.App;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class RoundTask extends AsyncTask<Void, Integer, Boolean> {
+
+	private static final String TOAST_MSG = "Calcul de l'itinéraire en cours...";
+	private static final String TOAST_ERR_MAJ = "Impossible de trouver un itinéraire pour ce parcours !";
+
+	private GoogleMap map;
+	private Location location;
+	private String startStep;
+	private String endStep;
+	private final ArrayList<LatLng> listLatLng = new ArrayList<LatLng>();
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param map
+	 * @param locationSource
+	 */
+	public RoundTask(final GoogleMap map, final Location location) {
+		this.map = map;
+		this.location = location;
+		Log.i("Gmap", location.toString());
+		// Get the current location.
+		startStep = location.getLatitude()+","+location.getLongitude();
+		
+		// Get the location of the enterprise.
+		endStep = "Marseille";// TODO Faire un système de settings permettant de définir l'emplacement de l'entreprise.
+		
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void onPreExecute() {
+		Toast.makeText(App.context, TOAST_MSG, Toast.LENGTH_LONG).show();
+	}
+
+	/***
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected Boolean doInBackground(Void... params) {
+		try {
+			// Construction de l'url à appeler
+			final StringBuilder url = new StringBuilder("http://maps.googleapis.com/maps/api/directions/xml?sensor=false&language=fr");
+			url.append("&origin=");
+			url.append(startStep.replace(' ', '+'));
+			url.append("&destination=");
+			url.append(endStep.replace(' ', '+'));
+
+			// Appel du web service
+			final InputStream stream = new URL(url.toString()).openStream();
+
+			// Traitement des données
+			final DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+			documentBuilderFactory.setIgnoringComments(true);
+
+			final DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+
+			final Document document = documentBuilder.parse(stream);
+			document.getDocumentElement().normalize();
+
+			// On récupère d'abord le status de la requête
+			final String status = document.getElementsByTagName("status").item(0).getTextContent();
+			if (!"OK".equals(status)) {
+				return false;
+			}
+
+			// On récupère les steps
+			final Element elementLeg = (Element) document.getElementsByTagName("leg").item(0);
+			final NodeList nodeListStep = elementLeg.getElementsByTagName("step");
+			final int length = nodeListStep.getLength();
+
+			for (int i = 0; i < length; i++) {
+				final Node nodeStep = nodeListStep.item(i);
+
+				if (nodeStep.getNodeType() == Node.ELEMENT_NODE) {
+					final Element elementStep = (Element) nodeStep;
+
+					// On décode les points du XML
+					decodePolylines(elementStep.getElementsByTagName("points").item(0).getTextContent());
+				}
+			}
+
+			return true;
+		} catch (final Exception e) {
+			return false;
+		}
+	}
+
+	/**
+	 * Méthode qui décode les points en latitudes et longitudes
+	 * 
+	 * @param points
+	 */
+	private void decodePolylines(final String encodedPoints) {
+		int index = 0;
+		int lat = 0, lng = 0;
+
+		while (index < encodedPoints.length()) {
+			int b, shift = 0, result = 0;
+
+			do {
+				b = encodedPoints.charAt(index++) - 63;
+				result |= (b & 0x1f) << shift;
+				shift += 5;
+			} while (b >= 0x20);
+
+			int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+			lat += dlat;
+			shift = 0;
+			result = 0;
+
+			do {
+				b = encodedPoints.charAt(index++) - 63;
+				result |= (b & 0x1f) << shift;
+				shift += 5;
+			} while (b >= 0x20);
+
+			int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+			lng += dlng;
+
+			listLatLng.add(new LatLng((double) lat / 1E5, (double) lng / 1E5));
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void onPostExecute(final Boolean result) {
+		if (!result) {
+			Toast.makeText(App.context, TOAST_ERR_MAJ, Toast.LENGTH_SHORT).show();
+		} else {
+			// On déclare le polyline, c'est-à-dire le trait (ici bleu) que l'on ajoute sur la carte pour tracer l'itinéraire
+			final PolylineOptions polylines = new PolylineOptions();
+			polylines.color(Color.BLUE);
+
+			// On construit le polyline
+			for (final LatLng latLng : listLatLng) {
+				polylines.add(latLng);
+			}
+
+			// On déclare un marker vert que l'on placera sur le départ
+			final MarkerOptions markerA = new MarkerOptions();
+			markerA.position(listLatLng.get(0));
+			markerA.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+
+			// On déclare un marker rouge que l'on mettra sur l'arrivée
+			final MarkerOptions markerB = new MarkerOptions();
+			markerB.position(listLatLng.get(listLatLng.size() - 1));
+			markerB.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+
+			// On met à jour la carte
+			map.moveCamera(CameraUpdateFactory.newLatLngZoom(listLatLng.get(0), 10));
+			map.addMarker(markerA);
+			map.addPolyline(polylines);
+			map.addMarker(markerB);
+		}
+	}
+
+	/**
+	 * Get the name of the city by Location.
+	 * 
+	 * @param location
+	 * @return String
+	 */
+	public static String getCityNameByLocation(Location location) {
+		String cityName = null;
+		Geocoder gcd = new Geocoder(App.context, Locale.getDefault());
+		List<Address> addresses;
+		try {
+			addresses = gcd.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+			if (addresses.size() > 0)
+				System.out.println(addresses.get(0).getLocality());
+			cityName = addresses.get(0).getLocality();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return cityName;
+	}
+}
