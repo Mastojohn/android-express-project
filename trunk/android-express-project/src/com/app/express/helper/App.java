34a64d3811ab939e7ea635e8ed5949dec102d339
julien.baldy13@gmail.com <julien.baldy13@gmail.com@1c1b8a7e-04e1-d7a9-97cf-1652473ac422>
2013-05-23 12:50:01 +0000

    Ajout setCurrentRound. Refresh before get.

--- trunk/android-express-project/src/com/app/express/helper/App.java
+++ trunk/android-express-project/src/com/app/express/helper/App.java
@@ -1,5 +1,7 @@
 package com.app.express.helper;
 
+import java.sql.SQLException;
+
 import com.app.express.db.DatabaseHelper;
 import com.app.express.db.persistence.Round;
 
@@ -13,14 +15,25 @@ import android.content.Context;
 public class App {
 	public static Context context;
 	public static DatabaseHelper dbHelper;
-	public static Round currentRound;
+	private static Round currentRound;
 	
 	public static Round getCurrendRound(){
 		if(currentRound != null){
+			try {
+				// Refresh before returns.
+				dbHelper.getRoundDao().refresh(currentRound);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
 			return currentRound;
 		}else{
 			//TODO Récupérer la dernière tournée en bdd avec check de la base du jour.
 			return null;
 		}
+	}
+	
+	public static void setCurrentRound(Round round){
+		currentRound = round;
 	}
 }
