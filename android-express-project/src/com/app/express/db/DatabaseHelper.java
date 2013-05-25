20fb6089e9ba0d5debf6fe52d30c88b85aaec4b7
ain <ambroise.dhenain@gmail.com>
2013-05-25 02:06:28 +0000

    Ajout d'un cache pour le fichier XML. Démarrage de l'application en multi thread réduit à une dizaine de secondes ;)

--- android-express-project/src/com/app/express/db/DatabaseHelper.java
+++ android-express-project/src/com/app/express/db/DatabaseHelper.java
@@ -33,7 +33,7 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
 	/**
 	 * Version of the database. Must be changed when models persistence are changed for regenerated all tables.
 	 */
-	private static final int DATABASE_VERSION = 45;
+	private static final int DATABASE_VERSION = 47;
 
 	/**
 	 * DAO for deliverer instance.
