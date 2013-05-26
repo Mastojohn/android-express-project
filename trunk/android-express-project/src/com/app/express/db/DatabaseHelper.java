6c077fc7db9d9b1ed5d7b54acb07f5246cbd9416
ambroise.dhenain@gmail.com <ambroise.dhenain@gmail.com@1c1b8a7e-04e1-d7a9-97cf-1652473ac422>
2013-05-26 20:41:12 +0000

    Modification du manifest, lancement du programme sur la partie de liste des livraisons.

--- trunk/android-express-project/src/com/app/express/db/DatabaseHelper.java
+++ trunk/android-express-project/src/com/app/express/db/DatabaseHelper.java
@@ -33,7 +33,7 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
 	/**
 	 * Version of the database. Must be changed when models persistence are changed for regenerated all tables.
 	 */
-	private static final int DATABASE_VERSION = 54;
+	private static final int DATABASE_VERSION = 55;
 
 	/**
 	 * DAO for deliverer instance.
