196b39c2e2355d26b9d3e9a9cb52f6aa515c745f
ain <ambroise.dhenain@gmail.com>
2013-05-27 03:24:19 +0000

    Sauvegarde de la livraison en bdd.

--- android-express-project/src/com/app/express/activity/SignatureReceiver.java
+++ android-express-project/src/com/app/express/activity/SignatureReceiver.java
@@ -1,10 +1,11 @@
 package com.app.express.activity;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
- 
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.ContextWrapper;
@@ -51,322 +52,263 @@ import android.view.Menu;
 import android.view.View;
 
 public class SignatureReceiver extends Activity {
-    LinearLayout mContent;
-    signature mSignature;
-    Button mClear, mGetSign, mCancel, mSetSign;
-    public static String tempDir;
-    public int count = 1;
-    public String current = null;
-    private Bitmap mBitmap;
-    View mView;
-    File mypath;
-    private int deliveryId;
-    private Delivery delivery;
- 
-    private String uniqueId;
+	LinearLayout mContent;
+	signature mSignature;
+	Button mClear, mGetSign, mCancel, mSetSign;
+	public static String tempDir;
+	public int count = 1;
+	public String current = null;
+	private Bitmap mBitmap;
+	View mView;
+	File mypath;
+	private int deliveryId;
+	private Delivery delivery;
 
-    // METHODE static qui prend une image et la convertie en string (base64) et return le string.
-    public static String encodeTobase64(ByteArrayOutputStream baos)
-    { 
-        byte[] b = baos.toByteArray();
-        String imageEncoded = Base64.encodeToString(b,Base64.DEFAULT);
-        return imageEncoded;
-    }
-    
-    public static Bitmap decodeBase64(String input) 
-    {
-        byte[] decodedByte = Base64.decode(input, 0);
-        return BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length); 
-    }
-    
-    @SuppressWarnings("deprecation")
+	private String uniqueId;
+
 	@Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		// Initialize helpers.
 		App.context = getApplicationContext();
 		App.dbHelper = OpenHelperManager.getHelper(this, DatabaseHelper.class);
-		//récuperation de l'id de la livraison
-			Bundle extras = getIntent().getExtras();
-			deliveryId = extras.getInt("deliveryId");		
-		//end
-        setContentView(R.layout.activity_signature_receiver_second);
-        
-        tempDir = Environment.getExternalStorageDirectory() + "/" + getResources().getString(R.string.external_dir) + "/";
-        ContextWrapper cw = new ContextWrapper(getApplicationContext());
-        File directory = cw.getDir(getResources().getString(R.string.external_dir), Context.MODE_PRIVATE);
- 
-        prepareDirectory();
-       // uniqueId = getTodaysDate() + "_" + getCurrentTime() + "_" + Math.random();
-//        current = uniqueId + ".png";
-//        mypath= new File(directory,current);
- 
- 
-        mContent = (LinearLayout) findViewById(R.id.linearLayout);
-        
-        //signature
-        mSignature = new signature(this, null);
-        mSignature.setBackgroundColor(Color.WHITE);
-        
-        mContent.addView(mSignature, LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
-        mClear = (Button)findViewById(R.id.clear);
-        mGetSign = (Button)findViewById(R.id.getsign);
-        mSetSign = (Button)findViewById(R.id.setsign);
-        mGetSign.setEnabled(false);
-      //  mCancel = (Button)findViewById(R.id.cancel);
-        mView = mContent;
- 
-        //yourName = (EditText) findViewById(R.id.yourName);
- 
-        mClear.setOnClickListener(new OnClickListener()
-        {       
-            public void onClick(View v)
-            {
-                Log.v("log_tag", "Panel Cleared");
-                mSignature.clear();
-                mGetSign.setEnabled(false);
-            }
-        });
-        
-        mGetSign.setOnClickListener(new OnClickListener()
-        {       
-            public void onClick(View v)
-            {
-                Log.v("log_tag", "Panel Saved");
-                boolean error = captureSignature();
-                if(!error){
-                    mView.setDrawingCacheEnabled(true);
-                    mSignature.save(mView);
-                    Bundle b = new Bundle();
-                    b.putString("status", "done");
-                    Intent intent = new Intent();
-                    intent.putExtras(b);
-                    setResult(RESULT_OK,intent);  
-                    finish();
-                }
-            }
-        });
-    }
-    
-    
- 
-    @Override
-    protected void onDestroy() {
-        Log.w("GetSignature", "onDestory");
-        super.onDestroy();
-    }
- 
-    private boolean captureSignature() {
- 
-        boolean error = false;
-        String errorMessage = ""; 
-        if(error){
-            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
-            toast.setGravity(Gravity.TOP, 105, 50);
-            toast.show();
-        }
-         return error;
-    }
- 
-    private boolean prepareDirectory()
-    {
-        try
-        {
-            if (makedirs())
-            {
-                return true;
-            } else {
-                return false;
-            }
-        } catch (Exception e)
-        {
-            e.printStackTrace();
-            Toast.makeText(this, "Could not initiate File System.. Is Sdcard mounted properly?", 1000).show();
-            return false;
-        }
-    }
- 
-    private boolean makedirs()
-    {
-        File tempdir = new File(tempDir);
-        if (!tempdir.exists())
-            tempdir.mkdirs();
- 
-        if (tempdir.isDirectory())
-        {
-            File[] files = tempdir.listFiles();
-            for (File file : files)
-            {
-                if (!file.delete())
-                {
-                    System.out.println("Failed to delete " + file);
-                }
-            }
-        }
-        return (tempdir.isDirectory());
-    }
- 
-    public class signature extends View
-    {
-        private static final float STROKE_WIDTH = 5f;
-        private static final float HALF_STROKE_WIDTH = STROKE_WIDTH / 2;
-        private Paint paint = new Paint();
-        private Path path = new Path();
- 
-        private float lastTouchX;
-        private float lastTouchY;
-        private final RectF dirtyRect = new RectF();
- 
-        public signature(Context context, AttributeSet attrs)
-        {
-            super(context, attrs);
-            paint.setAntiAlias(true);
-            paint.setColor(Color.BLACK);
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setStrokeJoin(Paint.Join.ROUND);
-            paint.setStrokeWidth(STROKE_WIDTH);
-        }
+		// récuperation de l'id de la livraison
+		Bundle extras = getIntent().getExtras();
+		deliveryId = extras.getInt("deliveryId");
+		// end
+		setContentView(R.layout.activity_signature_receiver_second);
 
- 
-        // Sauvegarde l'image non pas en .PNG mais en STRING 
-        public String save(View v)
-      {
-//            Log.v("log_tag", "Width: " + v.getWidth());
-//            Log.v("log_tag", "Height: " + v.getHeight());
-            XmlParser xml = new XmlParser();
-         
-            String newImg = null;
-            if(mBitmap == null)
-            {
-                mBitmap =  Bitmap.createBitmap (mContent.getWidth(), mContent.getHeight(), Bitmap.Config.RGB_565);;
-            }
-            Canvas canvas = new Canvas(mBitmap);
-            try
-            {
-            	ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                v.draw(canvas);
-                mBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
-                newImg = encodeTobase64(baos);
-                Log.e("String BITMAP ->", newImg);
-                
-            }
-            catch(Exception e)
-            {
-                Log.v("log_tag", e.toString());
-            }
-            //xml.WriteXML(getApplicationContext(), newImg);
-//          // TESTER LAJOUT EN BDD !!
-//			 
-	          try {
-					//Récupération de la livraison 
-					Dao<Delivery, Integer> deliveryDao = App.dbHelper.getDeliveryDao();
-					delivery = deliveryDao.queryForId(deliveryId);
-					Log.i("delivery_premier", delivery.toString());
-					delivery.setSignature(newImg);
-					delivery.update();
-					Dao<Delivery, Integer> deliverytest= App.dbHelper.getDeliveryDao();
-					delivery = deliverytest.queryForId(deliveryId);
-					Log.i("delivery_modif", delivery.getSignature());
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} 
-          
-            return newImg;
-        }
+		tempDir = Environment.getExternalStorageDirectory() + "/" + getResources().getString(R.string.external_dir) + "/";
+		ContextWrapper cw = new ContextWrapper(getApplicationContext());
+		File directory = cw.getDir(getResources().getString(R.string.external_dir), Context.MODE_PRIVATE);
 
-  
-        public void clear()
-        {
-            path.reset();
-            invalidate();
-        }
- 
-        @Override
-        protected void onDraw(Canvas canvas)
-        {
-            canvas.drawPath(path, paint);
-        }
- 
-        @Override
-        public boolean onTouchEvent(MotionEvent event)
-        {
-            float eventX = event.getX();
-            float eventY = event.getY();
-            mGetSign.setEnabled(true);
- 
-            switch (event.getAction())
-            {
-            case MotionEvent.ACTION_DOWN:
-                path.moveTo(eventX, eventY);
-                lastTouchX = eventX;
-                lastTouchY = eventY;
-                return true;
- 
-            case MotionEvent.ACTION_MOVE:
- 
-            case MotionEvent.ACTION_UP:
- 
-                resetDirtyRect(eventX, eventY);
-                int historySize = event.getHistorySize();
-                for (int i = 0; i < historySize; i++)
-                {
-                    float historicalX = event.getHistoricalX(i);
-                    float historicalY = event.getHistoricalY(i);
-                    expandDirtyRect(historicalX, historicalY);
-                    path.lineTo(historicalX, historicalY);
-                }
-                path.lineTo(eventX, eventY);
-                break;
- 
-            default:
-                debug("Ignored touch event: " + event.toString());
-                return false;
-            }
- 
-            invalidate((int) (dirtyRect.left - HALF_STROKE_WIDTH),
-                    (int) (dirtyRect.top - HALF_STROKE_WIDTH),
-                    (int) (dirtyRect.right + HALF_STROKE_WIDTH),
-                    (int) (dirtyRect.bottom + HALF_STROKE_WIDTH));
- 
-            lastTouchX = eventX;
-            lastTouchY = eventY;
- 
-            return true;
-        }
- 
-        private void debug(String string){
-        }
- 
-        private void expandDirtyRect(float historicalX, float historicalY)
-        {
-            if (historicalX < dirtyRect.left)
-            {
-                dirtyRect.left = historicalX;
-            }
-            else if (historicalX > dirtyRect.right)
-            {
-                dirtyRect.right = historicalX;
-            }
- 
-            if (historicalY < dirtyRect.top)
-            {
-                dirtyRect.top = historicalY;
-            }
-            else if (historicalY > dirtyRect.bottom)
-            {
-                dirtyRect.bottom = historicalY;
-            }
-        }
- 
-        private void resetDirtyRect(float eventX, float eventY)
-        {
-            dirtyRect.left = Math.min(lastTouchX, eventX);
-            dirtyRect.right = Math.max(lastTouchX, eventX);
-            dirtyRect.top = Math.min(lastTouchY, eventY);
-            dirtyRect.bottom = Math.max(lastTouchY, eventY);
-        }
-    }
+		prepareDirectory();
+		// uniqueId = getTodaysDate() + "_" + getCurrentTime() + "_" + Math.random();
+		// current = uniqueId + ".png";
+		// mypath= new File(directory,current);
+
+		mContent = (LinearLayout) findViewById(R.id.linearLayout);
+
+		// signature
+		mSignature = new signature(this, null);
+		mSignature.setBackgroundColor(Color.WHITE);
+
+		mContent.addView(mSignature, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+		mClear = (Button) findViewById(R.id.clear);
+		mGetSign = (Button) findViewById(R.id.getsign);
+		mSetSign = (Button) findViewById(R.id.setsign);
+		mGetSign.setEnabled(false);
+		// mCancel = (Button)findViewById(R.id.cancel);
+		mView = mContent;
+
+		// yourName = (EditText) findViewById(R.id.yourName);
+
+		mClear.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Log.v("log_tag", "Panel Cleared");
+				mSignature.clear();
+				mGetSign.setEnabled(false);
+			}
+		});
+
+		mGetSign.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Log.v("log_tag", "Panel Saved");
+				boolean error = captureSignature();
+				if (!error) {
+					mView.setDrawingCacheEnabled(true);
+					String signature = mSignature.save(mView);
+					Bundle b = new Bundle();
+					b.putString("status", "done");
+					b.putString("signature", signature);
+					Intent intent = new Intent();
+					intent.putExtras(b);
+					setResult(RESULT_OK, intent);
+					finish();
+				}
+			}
+		});
+	}
+
+	@Override
+	protected void onDestroy() {
+		Log.w("GetSignature", "onDestory");
+		super.onDestroy();
+	}
+
+	private boolean captureSignature() {
+
+		boolean error = false;
+		String errorMessage = "";
+		if (error) {
+			Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+			toast.setGravity(Gravity.TOP, 105, 50);
+			toast.show();
+		}
+		return error;
+	}
+
+	private boolean prepareDirectory() {
+		try {
+			if (makedirs()) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			Toast.makeText(this, "Could not initiate File System.. Is Sdcard mounted properly?", 1000).show();
+			return false;
+		}
+	}
+
+	private boolean makedirs() {
+		File tempdir = new File(tempDir);
+		if (!tempdir.exists())
+			tempdir.mkdirs();
+
+		if (tempdir.isDirectory()) {
+			File[] files = tempdir.listFiles();
+			for (File file : files) {
+				if (!file.delete()) {
+					System.out.println("Failed to delete " + file);
+				}
+			}
+		}
+		return (tempdir.isDirectory());
+	}
+
+	// METHODE static qui prend une image et la convertie en string (base64) et return le string.
+	public static String encodeTobase64(ByteArrayOutputStream baos) {
+		byte[] b = baos.toByteArray();
+		String imageEncoded = Base64.encodeToString(b, Base64.DEFAULT);
+		return imageEncoded;
+	}
+
+	public static Bitmap decodeBase64(String input) {
+		byte[] decodedByte = Base64.decode(input, 0);
+		return BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length);
+	}
+
+	public class signature extends View {
+		private static final float STROKE_WIDTH = 5f;
+		private static final float HALF_STROKE_WIDTH = STROKE_WIDTH / 2;
+		private Paint paint = new Paint();
+		private Path path = new Path();
+
+		private float lastTouchX;
+		private float lastTouchY;
+		private final RectF dirtyRect = new RectF();
+
+		public signature(Context context, AttributeSet attrs) {
+			super(context, attrs);
+			paint.setAntiAlias(true);
+			paint.setColor(Color.BLACK);
+			paint.setStyle(Paint.Style.STROKE);
+			paint.setStrokeJoin(Paint.Join.ROUND);
+			paint.setStrokeWidth(STROKE_WIDTH);
+		}
+
+		// Sauvegarde l'image non pas en .PNG mais en STRING
+		public String save(View v) {
+			// Log.v("log_tag", "Width: " + v.getWidth());
+			// Log.v("log_tag", "Height: " + v.getHeight());
+			XmlParser xml = new XmlParser();
+
+			String newImg = null;
+			if (mBitmap == null) {
+				mBitmap = Bitmap.createBitmap(mContent.getWidth(), mContent.getHeight(), Bitmap.Config.RGB_565);
+			}
+			
+			Canvas canvas = new Canvas(mBitmap);
+			try {
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				v.draw(canvas);
+				mBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+				newImg = encodeTobase64(baos);
+				Log.i("String BITMAP ->", newImg);
+
+			} catch (Exception e) {
+				Log.w("log_tag", e.toString());
+			}
+
+			return newImg;
+		}
+
+		public void clear() {
+			path.reset();
+			invalidate();
+		}
+
+		@Override
+		protected void onDraw(Canvas canvas) {
+			canvas.drawPath(path, paint);
+		}
+
+		@Override
+		public boolean onTouchEvent(MotionEvent event) {
+			float eventX = event.getX();
+			float eventY = event.getY();
+			mGetSign.setEnabled(true);
+
+			switch (event.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					path.moveTo(eventX, eventY);
+					lastTouchX = eventX;
+					lastTouchY = eventY;
+					return true;
+
+				case MotionEvent.ACTION_MOVE:
+
+				case MotionEvent.ACTION_UP:
+
+					resetDirtyRect(eventX, eventY);
+					int historySize = event.getHistorySize();
+					for (int i = 0; i < historySize; i++) {
+						float historicalX = event.getHistoricalX(i);
+						float historicalY = event.getHistoricalY(i);
+						expandDirtyRect(historicalX, historicalY);
+						path.lineTo(historicalX, historicalY);
+					}
+					path.lineTo(eventX, eventY);
+					break;
+
+				default:
+					debug("Ignored touch event: " + event.toString());
+					return false;
+			}
+
+			invalidate((int) (dirtyRect.left - HALF_STROKE_WIDTH), (int) (dirtyRect.top - HALF_STROKE_WIDTH), (int) (dirtyRect.right + HALF_STROKE_WIDTH), (int) (dirtyRect.bottom + HALF_STROKE_WIDTH));
+
+			lastTouchX = eventX;
+			lastTouchY = eventY;
+
+			return true;
+		}
+
+		private void debug(String string) {
+		}
+
+		private void expandDirtyRect(float historicalX, float historicalY) {
+			if (historicalX < dirtyRect.left) {
+				dirtyRect.left = historicalX;
+			} else if (historicalX > dirtyRect.right) {
+				dirtyRect.right = historicalX;
+			}
+
+			if (historicalY < dirtyRect.top) {
+				dirtyRect.top = historicalY;
+			} else if (historicalY > dirtyRect.bottom) {
+				dirtyRect.bottom = historicalY;
+			}
+		}
+
+		private void resetDirtyRect(float eventX, float eventY) {
+			dirtyRect.left = Math.min(lastTouchX, eventX);
+			dirtyRect.right = Math.max(lastTouchX, eventX);
+			dirtyRect.top = Math.min(lastTouchY, eventY);
+			dirtyRect.bottom = Math.max(lastTouchY, eventY);
+		}
+	}
 }
